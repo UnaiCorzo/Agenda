74a302123aac6c24e8b02b79a0e69f217ab89c0a
UnaiCorzo <74232809+UnaiCorzo@users.noreply.github.com>
2021-10-24 17:11:51 +0200

    Fix whitespace

--- src/agenda2/agenda.php
+++ src/agenda2/agenda.php
@@ -14,7 +14,7 @@
     if (!isset($_COOKIE['username'])) {
         $username = htmlentities($_POST['username']);
         setcookie('username', json_encode($username), 0);
-        echo "<h1>Agenda de" . $username . "</h1>";
+        echo "<h1>Agenda de " . $username . "</h1>";
     } else {
         echo "<h1>Agenda de " . json_decode($_COOKIE['username'], true) . "</h1>";
     }
